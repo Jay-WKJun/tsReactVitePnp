8221f1d29ca2a4255fc2231fb1f2f26a1a3fab74
Woo KyungJun <natcho9010@gmail.com>
2022-10-17 21:14:26 +0900

    chore: add comment in commitlintrc

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,8 @@
+// commit lint rules
 // https://commitlint.js.org/#/reference-rules
+
 module.exports = {
+  // conventional commit
   // https://www.conventionalcommits.org/en/v1.0.0/
   extends: ['@commitlint/config-conventional'],
 };
